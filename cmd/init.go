735b7a7c5b6b0a519777f4dcd3992289ec292690
Elijah J. Passmore <elijah@elijahjpassmore.com>
2021-09-10 00:47:15 +1200

    Fix init header

--- cmd/init.go
+++ cmd/init.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 Elijah J. Passmore <elijah@elijahjpassmore.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
